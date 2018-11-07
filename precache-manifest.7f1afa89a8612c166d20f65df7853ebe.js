169c283f91ecad0d2b54f0974c5643a0dd716d95
soo-park <sparkstacks@gmail.com>
2018-11-06 23:47:42 -0800

    Updates

---
+++ precache-manifest.7f1afa89a8612c166d20f65df7853ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba4b7547f3f3da95ba5f",
+    "url": "/static/css/main.30911760.chunk.css"
+  },
+  {
+    "revision": "ba4b7547f3f3da95ba5f",
+    "url": "/static/js/main.ba4b7547.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e339a600e59d71725d78aba7d3b2fdef",
+    "url": "/index.html"
+  }
+];
